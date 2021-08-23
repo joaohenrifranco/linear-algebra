49075f398adf9bf84331eca42dad511da559a5f9
João Henrique Franco <joaohenriquef@poli.ufrj.br>
2021-08-23 18:54:07 -0300

    Add jacobi solver

---
+++ src/linear-algebra/jacobi.py
@@ -0,0 +1,32 @@
+import numpy as np
+import math
+import checker
+
+def solve_jacobi(A, B, rtol):
+    n = A.shape[0]
+    residue = rtol + 1
+    X = np.ones(n)
+
+    if (not checker.is_diagonal_dominant(A)):
+        raise Exception(
+            "Jacobi method diverges, matrix is not diagonal dominant"
+        )
+
+    while(residue > rtol):
+        X_current = np.ones(n)
+
+        for i in range(n):
+            subtrahend = 0
+
+            for j in range(n):
+                if (j == i):
+                    continue
+                subtrahend += A[i, j] * X[j]
+
+            X_current[i] = (B[i] - subtrahend) / A[i, i]
+
+        residue = np.linalg.norm(X_current - X, ord=2) / \
+            np.linalg.norm(X_current, ord=2)
+        X = X_current
+
+    return X
